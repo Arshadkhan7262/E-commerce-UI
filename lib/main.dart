0016421896d2552e9e979198cf80730cd2e548a3
arshadkhan <arshadgopang7262@gmail.com>
2024-05-13 11:29:43 -0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:emart_app/Screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'consts/consts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: 'AIzaSyCwifqNw_dEoofUWlGeNOzx7nrdyhw864k',
+          appId: '1:773225546050:android:583f7b893f744fb5b208db',
+          messagingSenderId: '773225546050',
+          projectId: 'ecommerce-app-d8467',
+      storageBucket: 'gs://ecommerce-app-d8467.appspot.com'));
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +22,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: appname,
       theme: ThemeData(),
+      home: SplashScreen(),
     );
   }
 }
